fe3e4007f2b4fee3476fc40700daf846b0d83a51
Rye-Guy <ryanracioppo95@gmail.com>
2019-03-20 16:27:15 -0400

    imported tools for making api calls for  building proper urls with dynamicly generated links

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,17 @@
 ﻿import axios from 'axios';
-import urls from '../coolstuff.js'
+import api_tools from '../coolstuff.js'
 export const FETCH_PUBS = 'FETCH_PUBS'
 export const FETCH_EMBED_HTML = 'FETCH_EMBED_HTML'
 
 export function getPublications(){
-    const url = `${urls.root}`
-    console.log(urls)
-    const request = axios.get(url)
-    console.log('Request: ', request)
-    return {
-        type: FETCH_PUBS,
-        payload: request
-    };
+    
+   // const request = axios.get(url)
+   // console.log('Request: ', request)
+    console.log(api_tools)
+    // return {
+    //     type: FETCH_PUBS,
+    //     payload: request
+    // };
 }
 
 export function getHTMLSnippet(){
