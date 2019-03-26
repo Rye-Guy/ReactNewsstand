2479449d5d2eefd5bfb546bcc126f5d8a674394c
Rye-Guy <ryanracioppo95@gmail.com>
2019-03-26 17:05:55 -0400

    tools imported and being utilized properly

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,13 +5,15 @@ export const FETCH_EMBED_HTML = 'FETCH_EMBED_HTML'
 
 export function getPublications(){
     
-   // const request = axios.get(url)
-   // console.log('Request: ', request)
+    
     console.log(api_tools)
-    // return {
-    //     type: FETCH_PUBS,
-    //     payload: request
-    // };
+    let additional_params = {format:'json', pageSize: 30}
+    let params = new api_tools.create_base_parameters_obj('issuu.document_embeds.list', api_tools.api_key, additional_params)
+    console.log(params)
+    let api_call = api_tools.add_custom_params(params)
+    api_tools.call_issuu(api_call)
+    
+
 }
 
 export function getHTMLSnippet(){
